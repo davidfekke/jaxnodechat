2f34c3f4871bdb3d63426db8d94acf87913ba9a2
David Fekke <david@fekke.com>
2014-03-15 23:50:58 -0400

    Added Connection click

--- app.js
+++ app.js
@@ -3,18 +3,20 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
+var express = require('express')
+, app = express()
+, http = require('http')
+, server = http.createServer(app)
+, io = require('socket.io').listen(server);
 var path = require('path');
 
-var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'jaxnodechatsecret' }));
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -23,14 +25,17 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/public/index.html');
+});
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
