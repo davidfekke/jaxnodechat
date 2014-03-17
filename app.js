12a58a9afacaa7dbac8c919686deb152b96386bf
David Fekke <david@fekke.com>
2014-03-17 09:51:31 -0400

    Added disconnect
    Need to add reconnect functionality

--- app.js
+++ app.js
@@ -30,18 +30,44 @@ app.get('/', function(req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.on('addUser', function (data) {
-    console.log(data);
-	users.push(data.user);
-    console.log(socket.id);
-	io.sockets.emit('newUserAddedMessage', { name: data.user });
-    io.sockets.emit('usersInChat', { users: users });
-  });
-  socket.on('sendMessage', function(data) {
-	io.sockets.emit('chat', { user: data.user, message: data.message });
-  });
+	socket.on('addUser', function (data) {
+		console.log(data);
+		//users.push(data.user);
+		addToUsersArray(data.user);
+		io.sockets.emit('newUserAddedMessage', { name: data.user });
+		io.sockets.emit('usersInChat', { users: users });
+	});
+	socket.on('sendMessage', function(data) {
+		io.sockets.emit('chat', { user: data.user, message: data.message });
+	});
+	socket.on('disconnectUser', function (data) {
+		removeFromUsersArray(data.user);
+		io.sockets.emit('userRemovedMessage', { name: data.user});
+		socket.disconnect(true);
+	});
 });
 
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+function addToUsersArray(user) {
+	var userAlreadyExists = false;
+	for (var i = 0; i < users.length; i++)
+	{
+		if (user === users[i]){
+			userAlreadyExists = true;
+		}
+	}
+	if (userAlreadyExists === false)
+	{
+		users.push(user);
+	}
+}
+
+function removeFromUsersArray(user) {
+	var index = users.indexOf(user);
+	if (index > -1) {
+		users.splice(index, 1);
+	}
+}
